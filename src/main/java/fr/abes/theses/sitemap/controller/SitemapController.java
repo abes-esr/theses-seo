a4f84f4aadcb9ec8783c6c7563514618a0564767
gibert <gibert@abes.fr>
2023-04-11 11:17:28 +0200

    Ajout du service robots.txt

--- src/main/java/fr/abes/theses/sitemap/controller/SitemapController.java
+++ src/main/java/fr/abes/theses/sitemap/controller/SitemapController.java
@@ -8,11 +8,22 @@ import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Optional;
-
 @Slf4j
 @RestController
 @RequestMapping("/")
@@ -20,6 +31,9 @@ public class SitemapController {
 
     @Autowired
     SearchQueryBuilder searchQueryBuilder;
+
+    @Value("${robots.path}")
+    String robotsPath;
 
     @ResponseBody
     @GetMapping(value = "SiteMap", produces = MediaType.APPLICATION_XML_VALUE)
@@ -43,5 +57,14 @@ public class SitemapController {
             log.error(e.toString());
             throw e;
         }
+    }
+
+    @GetMapping(value = "/robots.txt", produces = "text/plain")
+    public ResponseEntity robots() throws IOException {
+        InputStream inputStream = new FileInputStream(new File(robotsPath));
+        InputStreamResource inputStreamResource = new InputStreamResource(inputStream);
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentLength(Files.size(Paths.get(robotsPath)));
+        return new ResponseEntity(inputStreamResource, headers, HttpStatus.OK);
     }
 }
